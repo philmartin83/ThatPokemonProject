ecd832c5ea80ffc63b0bbff620075f1f7574eeeb
Phil Martin <phil.martin1983@gmail.com>
2024-04-05 20:13:54 +0100

    task: Initial commit
    Add template/ empty project
    
    Refs #POK-001

---
+++ ThatPokemonProject/ThatPokemonProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThatPokemonProjectApp.swift
+//  ThatPokemonProject
+//
+//  Created by Phil Martin on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThatPokemonProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
